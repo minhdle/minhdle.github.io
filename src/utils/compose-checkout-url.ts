5521f51ee8378df042647d8a85cbb4c2a3f58ded
Minh Le <mile@bolt.com>
2023-03-07 14:04:20 -0600

    fix url

--- src/utils/compose-checkout-url.ts
+++ src/utils/compose-checkout-url.ts
@@ -3,7 +3,7 @@ import {CHECKOUT_URL_BASE, MERCHANT_DIVISION, PUBLISHER_KEY} from "../env";
 export function composeCheckoutUrl(quantitiesConfig: {
   [id: string]: number;
 }): string {
-  const entries: [string, number][] = Object.entries(quantitiesConfig).filter(pair => !pair[1]);
+  const entries: [string, number][] = Object.entries(quantitiesConfig).filter(pair => pair[1]);
   const skus = entries.map(pair => pair[0]).join(',');
   const quantities = entries.map(pair => pair[1]).join(',');
   return `${CHECKOUT_URL_BASE}?merchant_division_id=${MERCHANT_DIVISION}&publisher_key=${PUBLISHER_KEY}&sku=${skus}&quantity=${quantities}`;
