77411a56e221b2ba67378e10de98a602b01f1977
Minh Le <mile@bolt.com>
2023-03-07 10:41:28 -0600

    update API link

--- src/fetch/fetchProducts.ts
+++ src/fetch/fetchProducts.ts
@@ -1,13 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 import CryptoJS from "crypto-js";
-import { Product } from "../components/product-list/ProductList";
+import type { Product } from "../components/product-list/types";
 
 export interface ProductResp {
   BoltID: string;
   Name: string;
   Description: string;
   AvailabilityCount: number;
-  Prices: { UnitPriceInCents: number; Currency: string }[];
+  Prices: {
+    UnitPriceInCents: number;
+    Currency: string;
+    ListPriceInCents: number;
+  }[];
   Multimedia: {
     ImageURLs?: string[];
   };
@@ -36,34 +40,39 @@ export interface FetchProductsResp {
 export async function fetchProducts(): Promise<Product[]> {
   const nonce = uuidv4();
   const baseString = `:${nonce}`;
-  const stagingSecret = "test_sandbox";
+  const stagingSecret = "test_staging";
 
   const signatureBytes = CryptoJS.HmacSHA256(baseString, stagingSecret);
-  console.log(signatureBytes);
   const authSig = CryptoJS.enc.Base64.stringify(signatureBytes);
   const headers = new Headers();
-  headers.append("X-Publisher-Key", "fbf27e893e8f512fb39cd752b664af3d");
+  headers.append("X-Publisher-Key", "b61b9342d84f5a7c9aeea9b09574d16c");
   headers.append("X-Nonce", nonce);
   headers.append("X-Authorization-Signature", authSig);
 
   return fetch(
-    "https://api-sandbox.bolt.com/v1/products/catalog?merchant_division_id=LkYmU2HuLkws&limit=5",
+    "https://api-staging.bolt.com/v1/products/catalog?merchant_division_id=LkyzotSFu0PH&limit=20",
     { headers }
   )
     .then(async (res) => {
       const json: FetchProductsResp = await res.json();
 
       return json.Catalog.map((item) => {
-        return item.Current.Name ? item.Current : item.Parent;
+        return item.Current.Name ? item.Current : {
+          ...item.Current,
+          Name: item.Parent.Name,
+          Description: item.Parent.Description,
+          Multimedia: item.Parent.Multimedia
+        };
       });
     })
     .then((res) => {
       const parsedProducts = res.map((product) => {
         return {
-          price: product.Prices[0].UnitPriceInCents,
+          price: product.Prices[0].ListPriceInCents / 100,
           name: JSON.parse(product.Name).default,
           id: product.BoltID,
-          description: "",
+          description: JSON.parse(product.Description).default.slice(0, 30) + "...",
+          imageUrl: product.Multimedia.ImageURLs ? product.Multimedia.ImageURLs[0] : "",
         } as Product;
       });
       return parsedProducts;
